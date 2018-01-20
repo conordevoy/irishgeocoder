e012a339694f4c848f09777783eedf44fc637a73
Conor Devoy <conor.devoy@gmail.com>
2018-01-20 14:04:23 +0000

    Added structure, database, and client map

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/static")
+app.debug = True
 
 @app.route("/")
 def app_index():
-	return "Hello, World!"
+	return render_template("index.html")
 
 if __name__ == '__main__':
 	app.run()
---
+++ static/js/map.js
@@ -0,0 +1,2 @@
+var mymap = L.map('mapid').setView([53.34981, -6.26024], 12);
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap);
